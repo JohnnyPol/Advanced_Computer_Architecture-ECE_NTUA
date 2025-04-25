61972123e998044c84114613c152f1c36095d8c2
JohnnyPol <giannis.polychronopoulos@gmail.com>
2025-04-25 13:03:04 +0300

    Moved all the predictors to the branch_predictor.h file.

--- pintool/branch_predictor.h
+++ pintool/branch_predictor.h
@@ -280,4 +280,340 @@ private:
     UINT64 NumCorrectTargetPredictions;
 };
 
+class StaticAlwaysTakenPredictor : public BranchPredictor
+{
+public:
+    // Constructor
+    StaticAlwaysTakenPredictor() : BranchPredictor() {};
+
+    // Destructor
+    ~StaticAlwaysTakenPredictor() {};
+
+    // predict(): Always predicts Taken (true)
+    virtual bool predict(ADDRINT ip, ADDRINT target)
+    {
+        return true;
+    }
+
+    // update(): Updates counters but doesn't change predictor state
+    virtual void update(bool predicted, bool actual, ADDRINT ip, ADDRINT target)
+    {
+        // Update the prediction counters in the base class
+        updateCounters(predicted, actual);
+
+        // Static predictor, state does not change.
+        // ip and target arguments are ignored.
+    }
+
+    // getName(): Returns the name of the predictor
+    virtual string getName()
+    {
+        return "Static-AlwaysTaken";
+    }
+};
+
+class StaticBTFNTPredictor : public BranchPredictor
+{
+public:
+    StaticBTFNTPredictor() : BranchPredictor() {}
+    ~StaticBTFNTPredictor() {}
+
+    virtual bool predict(ADDRINT ip, ADDRINT target)
+    {
+        return target < ip;
+    }
+
+    virtual void update(bool predicted, bool actual, ADDRINT ip, ADDRINT target)
+    {
+        updateCounters(predicted, actual);
+    }
+
+    virtual string getName()
+    {
+        std::ostringstream stream;
+        stream << "BTFNT";
+        return stream.str();
+    }
+};
+
+class GlobalHistoryPredictor : public BranchPredictor
+{
+private:
+    const unsigned int pht_entries; // Αριθμός εγγραφών PHT (Z)
+    const unsigned int cntr_bits;   // Μήκος μετρητή PHT (X bits)
+    const unsigned int bhr_length;  // Μήκος Global BHR (N bits)
+
+    // Πίνακας PHT
+    std::vector<uint8_t> PHT; // Pattern History Table (κρατάει X-bit μετρητές)
+
+    // Καθολικός Καταχωρητής Ιστορικού
+    uint8_t BHR; // Branch History Register (κρατάει N bits ιστορικού)
+
+    // Μάσκες και όρια
+    const uint8_t counter_max;          // Μέγιστη τιμή μετρητή (2^X - 1)
+    const uint8_t bhr_mask;             // Μάσκα για τον BHR (2^N - 1)
+    const uint8_t prediction_threshold; // Όριο πρόβλεψης (2^(X-1))
+    const unsigned int pht_index_mask;  // Μάσκα για τον δείκτη PHT (Z-1)
+    const unsigned int pht_index_bits;  // Αριθμός bits για τον δείκτη PHT
+
+public:
+    // Constructor
+    GlobalHistoryPredictor(unsigned int pht_entries_Z, unsigned int counter_length_X, unsigned int bhr_length_N) : BranchPredictor(),
+                                                                                                                   pht_entries(pht_entries_Z),
+                                                                                                                   cntr_bits(counter_length_X),
+                                                                                                                   bhr_length(bhr_length_N),
+                                                                                                                   BHR(0), // Αρχικοποίηση BHR σε 0
+                                                                                                                   counter_max((1 << counter_length_X) - 1),
+                                                                                                                   bhr_mask((1 << bhr_length_N) - 1),
+                                                                                                                   prediction_threshold(1 << (counter_length_X - 1)),
+                                                                                                                   pht_index_mask(pht_entries_Z - 1),
+                                                                                                                   pht_index_bits(static_cast<unsigned int>(std::round(std::log2(pht_entries_Z))))
+    {
+        // Αρχικοποίηση PHT (μέγεθος Z)
+        // Αρχική κατάσταση: Weakly Not Taken (η κατάσταση ακριβώς κάτω από το όριο πρόβλεψης)
+        uint8_t initial_pht_state = (prediction_threshold > 0) ? prediction_threshold - 1 : 0;
+        PHT.assign(pht_entries, initial_pht_state);
+    }
+
+    ~GlobalHistoryPredictor() override = default;
+
+    // Μέθοδος πρόβλεψης
+    bool predict(ADDRINT ip, ADDRINT target) override
+    {
+        // Ολίσθηση του PC component για να κάνει χώρο για τα BHR bits
+        unsigned int shifted_pc_component = ip << bhr_length;
+
+        // Υπολογισμός του δείκτη PHT χρησιμοποιώντας το BHR
+        unsigned int pht_index = (shifted_pc_component | BHR) & pht_index_mask;
+
+        // Διάβασε τον X-bit μετρητή από τον PHT
+        uint8_t counter_state = PHT[pht_index];
+
+        // Κάνε την πρόβλεψη
+        uint8_t prediction = counter_state >> (cntr_bits - 1);
+        return (prediction != 0);
+    }
+
+    // Μέθοδος ενημέρωσης
+    void update(bool predicted, bool actual, ADDRINT ip, ADDRINT target) override
+    {
+        // Ολίσθηση του PC component για να κάνει χώρο για τα BHR bits
+        unsigned int shifted_pc_component = ip << bhr_length;
+
+        // Υπολογισμός του δείκτη PHT χρησιμοποιώντας το BHR
+        unsigned int pht_index = (shifted_pc_component | BHR) & pht_index_mask;
+
+        // 2. Ενημέρωσε τον X-bit μετρητή στον PHT
+        uint8_t old_counter_state = PHT[pht_index];
+        uint8_t new_counter_state;
+        if (actual) // Branch Taken
+        {
+            new_counter_state = (old_counter_state < counter_max) ? old_counter_state + 1 : counter_max;
+        }
+        else // Branch Not Taken
+        {
+            new_counter_state = (old_counter_state > 0) ? old_counter_state - 1 : 0;
+        }
+        PHT[pht_index] = new_counter_state;
+
+        unsigned int bhr_update_mask = (1 << (bhr_length - 1)); // Μάσκα για την ενημέρωση του BHR
+        // Ενημέρωσε τον καθολικό BHR
+        BHR = ((BHR >> 1) | ((actual) ? bhr_update_mask : 0)) & bhr_mask;
+
+        // Ενημέρωσε τους γενικούς μετρητές της βασικής κλάσης
+        updateCounters(predicted, actual);
+    }
+
+    // Μέθοδος για το όνομα του predictor
+    std::string getName() override
+    {
+        std::ostringstream stream;
+        stream << "Global"
+               << "-N" << bhr_length                     // BHR Length (N)
+               << "-X" << cntr_bits                      // Counter Bits (X)
+               << "-" << (pht_entries / 1024) << "KPHT"; // PHT Entries (Z in K)
+        return stream.str();
+    }
+};
+
+class LocalHistoryPredictor : public BranchPredictor
+{
+private:
+    // Παράμετροι του Predictor
+    const unsigned int bht_entries;      // Αριθμός εγγραφών BHT (X)
+    const unsigned int history_length;   // Μήκος ιστορικού ανά εγγραφή BHT (Z)
+    const unsigned int pht_entries;      // Αριθμός εγγραφών PHT (σταθερό 8192)
+    const unsigned int pht_counter_bits; // Bits ανά μετρητή PHT (σταθερό 2)
+    const unsigned int bht_length;       // Μήκος BHT (log2(X))
+    const unsigned int pht_index_mask;   // Μάσκα για τον δείκτη PHT (8192 - 1)
+
+    // Πίνακες
+    std::vector<uint8_t> BHT; // Branch History Table (κρατάει Z bits ιστορικού)
+    std::vector<uint8_t> PHT; // Pattern History Table (κρατάει 2-bit μετρητές)
+
+    // Μάσκα για το ιστορικό (για να κρατάμε μόνο Z bits)
+    const uint8_t history_mask;
+
+    // Όριο για τον 2-bit μετρητή
+    const uint8_t counter_max = 3; // (1 << pht_counter_bits) - 1;
+
+public:
+    // Constructor
+    LocalHistoryPredictor(unsigned int bht_entries_X, unsigned int history_length_Z) : BranchPredictor(),
+                                                                                       bht_entries(bht_entries_X),
+                                                                                       history_length(history_length_Z),
+                                                                                       pht_entries(8192),
+                                                                                       pht_counter_bits(2),
+                                                                                       pht_index_mask(8192 - 1),
+                                                                                       history_mask((1 << history_length_Z) - 1), // Υπολογισμός μάσκας
+                                                                                       bht_length(static_cast<unsigned int>(std::round(std::log2(bht_entries_X)))),
+    {
+        // Αρχικοποίηση BHT με μηδενικά (μέγεθος Χ)
+        BHT.assign(bht_entries, 0);
+
+        // Αρχικοποίηση PHT με 0 ή 1 (μέγεθος 8192)
+        // Ας πούμε Weakly Not Taken (1) ως αρχική κατάσταση
+        PHT.assign(pht_entries, 1);
+    }
+
+    ~LocalHistoryPredictor() override = default;
+
+    bool predict(ADDRINT ip, ADDRINT target) override
+    {
+        unsigned int bht_index = ip % bht_entries;
+        uint8_t local_history = BHT[bht_index];
+
+        // Ολίσθηση του PC component για να κάνει χώρο για τα BHR bits
+        unsigned int shifted_pc_component = ip << bht_length;
+
+        // Υπολογισμός του δείκτη PHT χρησιμοποιώντας το BHR
+        unsigned int pht_index = (shifted_pc_component | BHR) & pht_index_mask;
+
+        // Διάβασε τον 2-bit μετρητή από τον PHT
+        uint8_t counter_state = PHT[pht_index];
+
+        // Κάνε την πρόβλεψη (Taken αν >= 2)
+        bool prediction = (counter_state >= 2);
+        return prediction;
+    }
+
+    // Μέθοδος ενημέρωσης
+    void update(bool predicted, bool actual, ADDRINT ip, ADDRINT target) override
+    {
+        // Υπολόγισε δείκτη BHT
+        unsigned int bht_index = ip % bht_entries;
+
+        // Διάβασε το τοπικό ιστορικό (που χρησιμοποιήθηκε για την πρόβλεψη)
+        uint8_t local_history = BHT[bht_index];
+
+        // Ολίσθηση του PC component για να κάνει χώρο για τα BHR bits
+        unsigned int shifted_pc_component = ip << bht_length;
+
+        // Υπολογισμός του δείκτη PHT χρησιμοποιώντας το BHR
+        unsigned int pht_index = (shifted_pc_component | BHR) & pht_index_mask;
+
+        // Ενημέρωσε τον 2-bit μετρητή στον PHT
+        uint8_t old_counter_state = PHT[pht_index];
+        uint8_t new_counter_state;
+        if (actual)
+        {
+            new_counter_state = (old_counter_state < counter_max) ? old_counter_state + 1 : counter_max;
+        }
+        else
+        {
+            new_counter_state = (old_counter_state > 0) ? old_counter_state - 1 : 0;
+        }
+        PHT[pht_index] = new_counter_state;
+
+        unsigned int bht_update_mask = (1 << (bht_length - 1));
+        uint8_t new_history = ((local_history >> 1) | (actual ? bht_update_mask : 0)) & history_mask;
+        BHT[bht_index] = new_history;
+
+        updateCounters(predicted, actual);
+    }
+
+    // Μέθοδος για το όνομα του predictor
+    std::string getName() override
+    {
+        std::ostringstream stream;
+        stream << "Local-" << bht_entries << "ent-" << history_length << "hist";
+        return stream.str();
+    }
+};
+
+class Alpha21264Predictor : public TournamentHybridPredictor 
+{
+public:
+	Alpha21264Predictor() : TournamentHybridPredictor(12, new LocalHistoryPredictor(/* ? */), new GlobalHistoryPredictor(/* ? */)) {}
+	~Alpha21264Predictor() {}
+
+	virtual string getName() {
+		std::ostringstream stream;
+		stream << "Alpha21264";
+		return stream.str();
+	}
+};
+
+class TournamentHybridPredictor : public BranchPredictor
+{
+public:
+    TournamentHybridPredictor(unsigned int index_bits_, BranchPredictor *p1, BranchPredictor *p2) : BranchPredictor(), index_bits(index_bits_), predictor1(p1), predictor2(p2), table_entries(1 << index_bits)
+    {
+        TABLE = new unsigned long long[table_entries];
+        COUNTER_MAX = 3;
+        memset(TABLE, 0, table_entries * sizeof(*TABLE));
+    }
+
+    ~TournamentHybridPredictor()
+    {
+        delete[] TABLE;
+        delete predictor1;
+        delete predictor2;
+    }
+
+    virtual bool predict(ADDRINT ip, ADDRINT target)
+    {
+        unsigned int ip_table_index = ip % table_entries;
+        unsigned long long ip_table_value = TABLE[ip_table_index];
+        unsigned long long prediction_bit = (ip_table_value >> 1) & 1;
+
+        if (prediction_bit)
+            return (predictor2->predict(ip, target));
+        return (predictor1->predict(ip, target));
+    }
+
+    virtual void update(bool predicted, bool actual, ADDRINT ip, ADDRINT target)
+    {
+        unsigned int ip_table_index = ip % table_entries;
+        bool prediction1 = predictor1->predict(ip, target);
+        bool prediction2 = predictor2->predict(ip, target);
+
+        if (prediction1 != prediction2)
+        {
+            if (prediction1 == actual && (TABLE[ip_table_index] > 0))
+                TABLE[ip_table_index]--;
+            else if (TABLE[ip_table_index] < COUNTER_MAX)
+                TABLE[ip_table_index]++;
+        }
+        predictor1->update(prediction1, actual, ip, target);
+        predictor2->update(prediction2, actual, ip, target);
+        updateCounters(predicted, actual);
+    }
+
+    virtual string getName()
+    {
+        std::ostringstream stream;
+        stream << "Tournament-" << predictor1->getName() << "-" << predictor2->getName();
+        return stream.str();
+    }
+
+private:
+    unsigned int index_bits;
+    BranchPredictor *predictor1;
+    BranchPredictor *predictor2;
+    unsigned int table_entries;
+    unsigned long long *TABLE;
+    unsigned int COUNTER_MAX;
+};
+
 #endif
